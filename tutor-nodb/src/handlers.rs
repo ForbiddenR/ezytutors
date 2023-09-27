12a0bb0cd87417608adcc6d9148a5d5a5ae7c85c
ForbiddenR <hostyouposition@outlook.com>
2023-09-27 15:04:42 +0800

    add a post api

--- tutor-nodb/src/handlers.rs
+++ tutor-nodb/src/handlers.rs
@@ -1,4 +1,9 @@
+use std::convert::TryInto;
+
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
+
+use crate::models::Course;
 
 use super::state::AppState;
 
@@ -8,4 +13,52 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
     let response = format!("{} {} times", health_check_response, visit_count);
     *visit_count += 1;
     HttpResponse::Ok().json(&response)
+}
+
+pub async fn new_course(
+    new_course: web::Json<Course>,
+    app_state: web::Data<AppState>,
+) -> HttpResponse {
+    println!("Received new course");
+    let course_count_for_user = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .filter(|course| course.tutor_id == new_course.tutor_id)
+        .count();
+    let new_course = Course {
+        tutor_id: new_course.tutor_id,
+        course_id: Some((course_count_for_user + 1).try_into().unwrap()),
+        course_name: new_course.course_name.clone(),
+        posted_time: Some(Utc::now().naive_utc()),
+    };
+    app_state.courses.lock().unwrap().push(new_course);
+    HttpResponse::Ok().json("Added course")
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Mutex;
+
+    use super::*;
+    use actix_web::http::StatusCode;
+
+    #[actix_rt::test]
+    async fn post_course_test() {
+        let course = web::Json(Course {
+            tutor_id: 1,
+            course_name: "Hello, this is test course".into(),
+            course_id: None,
+            posted_time: None,
+        });
+        let app_state = web::Data::new(AppState{
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            courses: Mutex::new(vec![]),
+        });
+        let resp = new_course(course, app_state).await;
+        assert_eq!(resp.status(), StatusCode::OK)
+    }
 }
