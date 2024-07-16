45e64b4fad9095ec3264e50ea3720ea08170a311
ForbiddenR <hostyouposition@outlook.com>
2024-07-16 09:24:01 +0800

    add sqlx examples

--- tutor-db/src/bin/iter1.rs
+++ tutor-db/src/bin/iter1.rs
@@ -23,10 +23,21 @@ async fn main() -> io::Result<()> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
     println!("database_url {}", database_url);
     let db_pool = PgPool::connect(&database_url).await.unwrap();
-    println!("is closed {}", db_pool.is_closed());
-    sqlx::query!("").fetch_all(&db_pool)
-    .await
-    .unwrap();
-    
+    let course_rows = sqlx::query!(
+        r#"select course_id, tutor_id, course_name, posted_time from
+            ezy_course_c4 where course_id = $1"#,
+            1
+    ).fetch_all(&db_pool).await.unwrap();
+    let mut courses_list = vec![];
+    for course_row in course_rows {
+        courses_list.push(Course {
+            course_id: course_row.course_id,
+            tutor_id: course_row.tutor_id,
+            course_name: course_row.course_name,
+            posted_time: Some(chrono::NaiveDateTime::from(
+                course_row.posted_time.unwrap())),
+        })
+    }
+    println!("Courses = {:?}", courses_list);
     Ok(())
 }
