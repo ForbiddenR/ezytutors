51e0fb0c5219fd7e52daadb8835028d8c2bcf900
ForbiddenR <hostyouposition@outlook.com>
2024-07-16 11:39:14 +0800

    add a simple http web service

--- tutor-db/src/iter2/routes.rs
+++ tutor-db/src/iter2/routes.rs
@@ -1,6 +1,6 @@
 use actix_web::web;
 
-use super::handlers::{get_course_detail, get_courses_for_tutor, new_course};
+use super::handlers::{get_course_detail, get_courses_for_tutor, post_new_course};
 
 use super::handlers::health_check_handler;
 
@@ -11,7 +11,7 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
 pub fn course_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/courses")
-            .route("/", web::post().to(new_course))
+            .route("/", web::post().to(post_new_course))
             .route("/{tutor_id}", web::get().to(get_courses_for_tutor))
             .route("/{tutor_id}/{course_id}", web::get().to(get_course_detail)),
     );
